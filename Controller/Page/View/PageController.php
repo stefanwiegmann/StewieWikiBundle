fec6147df98eed5255f49c05f3cc99b05dceec65
Stefan Wiegmann <mail@stefanwiegmann.de>
2022-03-06 21:19:49 -0500

    added page view and page tree menu

---
+++ Controller/Page/View/PageController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Stewie\WikiBundle\Controller\Page\View;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+// use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+// use Stewie\UserBundle\Form\Type\User\DetailType;
+
+/**
+  * @IsGranted("ROLE_WIKI_PAGE_VIEW")
+  */
+
+class PageController extends AbstractController
+{
+    /**
+    * @Route("/page/view/space/{slug}", name="stewie_wiki_page_view_root")
+    */
+    public function root($slug, Request $request)
+    {
+      //get user
+      $em = $this->container->get('doctrine')->getManager();
+      $repo = $em->getRepository('StewieWikiBundle:Space');
+      $space = $repo->findOneBySlug($slug);
+
+      return $this->redirectToRoute('stewie_wiki_page_view_page', ['slug' => $space->getPage()->first()->getSlug()]);
+
+    }
+
+    /**
+    * @Route("/page/view/page/{slug}", name="stewie_wiki_page_view_page")
+    */
+    public function page($slug, Request $request)
+    {
+      //get user
+      $em = $this->container->get('doctrine')->getManager();
+      $repo = $em->getRepository('StewieWikiBundle:Page');
+      $page = $repo->findOneBySlug($slug);
+
+      return $this->render('@StewieWiki/page/view/page.html.twig', [
+          'page' => $page,
+          'space' => $page->getSpace(),
+      ]);
+    }
+}
